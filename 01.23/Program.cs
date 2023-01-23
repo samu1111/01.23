cbfa9e4aba5c30cbfa82bcb0713e4cacdb6206c9
Sámuel Konderla <samuelkonderla@gmail.com>
2023-01-23 08:41:06 +0100

    Add project files.

---
+++ 01.23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _01._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader be = new StreamReader("humming.txt");
+            string szo = "";
+            List<string> lista = new List<string>();
+            while (!be.EndOfStream)
+            {
+                
+                
+                
+                Console.WriteLine($"szó: {szo}");
+                Console.Write("lista: ");
+                foreach (var i in lista)
+                {
+                    Console.Write($"{i}");
+                }
+                Console.WriteLine();
+            }
+            be.Close();
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
